774f4a178337e2c4a8898234af4fab76d5cc7c0b
Suhavi Sandhu <sandhs11@mcmaster.ca>
2018-11-24 20:51:12 -0500

    fixed some stuff

--- Gemfile
+++ Gemfile
@@ -6,7 +6,8 @@ ruby '>= 2.3.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
+gem 'sqlite3', git: "https://github.com/larskanis/sqlite3-ruby", branch: "add-gemspec"
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
@@ -35,6 +36,13 @@ gem 'jbuilder', '~> 2.5'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+
+# Yummy UI
+gem 'bootstrap'
+gem 'jquery-rails'
+
+# Google Cloud
+gem 'google-cloud-vision'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/rainydays_controller.rb
+++ app/controllers/rainydays_controller.rb
@@ -1,6 +1,35 @@
+# Imports the Google Cloud client library
+require "google/cloud/vision"
+require 'open-uri'
+
 class RainydaysController < ApplicationController
 
     # This is our index page :)
     def index
+        
     end
+
+    def cloud_vision
+        img_url = params[:img_url]
+        puts img_url
+        # Your Google Cloud Platform project ID
+        project_id = ENV['PROJECT_ID']
+
+        # Instantiates a client
+        vision = Google::Cloud::Vision.new project: project_id
+
+        #image  = vision.image "./images/car.jpg"
+
+        open(img_url) do |img|
+          
+            image = vision.image(img)
+            puts image.text
+
+            annotation = vision.annotate(image, labels: true, text: true)
+            puts annotation
+
+          end
+        
+    end
+    
 end
