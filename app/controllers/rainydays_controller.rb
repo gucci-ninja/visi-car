7ffa277f032339d7d7c8030be470e8b97dede777
kawaiicheese <josephlu116@gmail.com>
2018-11-25 05:07:55 -0500

    finished SOAP

--- app/controllers/rainydays_controller.rb
+++ app/controllers/rainydays_controller.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 # SOAP library
 require 'savon'
 # REST library
+require 'httparty'
 # require 'net/http'
 # require 'uri'
 # require 'json'
@@ -45,35 +46,51 @@ class RainydaysController < ApplicationController
     end
 
     def style_extractor
-        input_string = ""
+        input_string = ["2016","Chrysler","200"]
 
-        client = Savon::Client.new do
-            wsdl.document = "http://services.chromedata.com/Description/7b?wsdl"
-        end
+        client = Savon.client(:wsdl => "http://services.chromedata.com/Description/7b?wsdl", :log => false)
 
-        response = client.request :urn, "VehicleDescriptionRequest" do
-            soap.body = {
-                ":urn:accountInfo" => nil, :attributes! => {
-                    :number => ENV['SOAP_NUMBER'],
-                    :secret => ENV['SOAP_SECRET'],
-                    :country => "CA",
-                    :language => "en"
+        response = client.call(
+            :describe_vehicle,
+            message: {
+                :account_info =>{
+                    :@number => "317789",
+                    :@secret =>  "d4297c7dc3d94bf3",
+                    :@country => "CA",
+                    :@language => "en"
                 },
-                "urn:modelYear" => input_string[0],
-                "urn:makeName" => input_string[1],
-                "urn:modelName" => input_string[2]
+                :model_year => input_string[0],
+                :make_name => input_string[1],
+                :model_name => input_string[2],
+                # :vin => "?",
             }
-        end
+        ).to_hash
+
+
+        return response[:vehicle_description][:style][0][:acode]
+        
+    end
+        # :secret => ENV['AUTO_SECRET'],
+        # :number => ENV['AUTO_USER'],
+
+    def best_deals
+        # location, car id
+        input_path="/%s/%s/" % ["97232", "378067"]
+        puts input_path
+
+        headers = { 
+            "Username"  => ENV['AUTO_USER'],
+            "Password" => ENV['AUTO_SECRET'] 
+        }
+        base_uri = URI.join("https://incentives.chromedata.com/BestOffer/offer/latest", input_path, "offers.json")
+        puts base_uri
+
+        request = HTTParty.get(
+            base_uri.to_s, 
+            :headers => headers
+        )
+        # do something with it
+        puts request.parsed_response
 
     end
-
-    # def best_deals
-    #     location=""
-    #     car_id=""
-
-    #     uri = URI.parse("https://incentives.chromedata.com/BestOffer/offer/latest/")
-    #     http = Net::HTTP.new(uri.host, uri.port)
-
-
-    # end
 end
