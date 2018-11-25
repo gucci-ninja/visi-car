c6308854f4535967252d7cc7e227125e4bd874c7
kawaiicheese <josephlu116@gmail.com>
2018-11-24 21:47:17 -0500

    SOAP get style

--- Gemfile
+++ Gemfile
@@ -44,6 +44,9 @@ gem 'jquery-rails'
 # Google Cloud
 gem 'google-cloud-vision'
 
+# Savon
+gem 'savon'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/rainydays_controller.rb
+++ app/controllers/rainydays_controller.rb
@@ -1,12 +1,13 @@
 # Imports the Google Cloud client library
 require "google/cloud/vision"
 require 'open-uri'
+require 'savon'
 
 class RainydaysController < ApplicationController
 
     # This is our index page :)
     def index
-        
+
     end
 
     def cloud_vision
@@ -21,7 +22,7 @@ class RainydaysController < ApplicationController
         #image  = vision.image "./images/car.jpg"
 
         open(img_url) do |img|
-          
+
             image = vision.image(img)
             puts image.text
 
@@ -29,7 +30,31 @@ class RainydaysController < ApplicationController
             puts annotation
 
           end
-        
+
     end
-    
+
+    def style_extractor
+        input_string = ""
+        puts 
+
+        client = Savon::Client.new do
+            wsdl.document = "http://services.chromedata.com/Description/7b?wsdl"
+        end
+
+        response = client.request :urn, "VehicleDescriptionRequest" do
+            soap.body = {
+                ":urn:accountInfo" => nil, :attributes! => {
+                    :number => ENV['SOAP_NUMBER'],
+                    :secret => ENV['SOAP_SECRET'],
+                    :country => "CA",
+                    :language => "en"
+                }
+                "urn:modelYear" => input_string[0]
+                "urn:makeName" => input_string[1]
+                "urn:modelName" => input_string[2]
+            }
+        end
+
+    end
+
 end
